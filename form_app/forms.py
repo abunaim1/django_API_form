4757affeb01a4e7003b3ebb653543f6c6cdfede2
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-18 18:32:10 +0600

    field add

--- form_app/forms.py
+++ form_app/forms.py
@@ -3,3 +3,13 @@ from django import forms
 class contactForm(forms.Form):
     name = forms.CharField(label='User Name')
     email = forms.EmailField(label='User Email')
+    age = forms.IntegerField()
+    weight = forms.FloatField()
+    balance = forms.DecimalField()
+    check = forms.BooleanField()
+    birthday = forms.DateField()
+    appoinment = forms.DateTimeField()
+    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    size = forms.ChoiceField(choices=CHOICES)
+    ITEM = [('P','Peparoni'),('B','Beef'),('M','Mashroom')]
+    ingradient = forms.MultipleChoiceField(choices=ITEM)
